0bfa16a8016269e1c406a99d4e8abd252e6bbbbe
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-05-07 16:55:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,3 @@
 class Song
-  attr_accessor :name, :artist, :genre
-  def initialize(name)
-    @name = name
-  end
-  def artist_name
-    if self.artist == nil
-      nil
-    else
-      self.artist.name
-    end
-  end
+  attr_accessor
 end
